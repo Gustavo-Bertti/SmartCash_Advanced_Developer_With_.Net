8b1ccfaf51bb1be953807b5074b28acab565863b
Gustavo Bertti <88666639+Gustavo-Bertti@users.noreply.github.com>
2024-03-12 19:21:20 -0300

    Update Faturamento.cs

--- SmartCash/Models/Faturamento.cs
+++ SmartCash/Models/Faturamento.cs
@@ -1,30 +1,11 @@
 ﻿namespace SmartCash.Models
 {
-       public class Faturamento : FluxoDeCaixa
-   {
-
-       private double _totalFaturamento;
-
-
-       public Faturamento(string tipo, double valor, string descricao, DateTime data) : base(tipo, valor, descricao, data)
+    public class Faturamento : FluxoDeCaixa
        {
-           if (tipo == "entrada") _totalFaturamento = valor;
-           else throw new ArgumentNullException();
-
-       }
-
-
-
-           protected double TotalFaturamento() 
-       {
-           ;
-           return _totalFaturamento;
        
+           public Faturamento(string tipo, double valor, string descricao, DateTime data) : base(tipo, valor, descricao, data)
+           {
+               if (tipo != "entrada") throw new ArgumentNullException();
+           }
        }
-
-       public override string ToString()
-       {
-           return $"Valor total do faturamento: {_totalFaturamento} reais";
-       }
-   }
 }
