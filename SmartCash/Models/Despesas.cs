eb2e74e4a95aa4e367392cfbb73f402995a1eae6
Gustavo Bertti <88666639+Gustavo-Bertti@users.noreply.github.com>
2024-03-12 19:20:38 -0300

    Update Despesas.cs

--- SmartCash/Models/Despesas.cs
+++ SmartCash/Models/Despesas.cs
@@ -1,25 +1,12 @@
 ﻿namespace SmartCash.Models
 {
-   public class Despesas : FluxoDeCaixa
-{
-    private double _totalDespesas { get; set; }
-
-    public Despesas(string tipo, double valor, string descricao, DateTime data) : base(tipo, valor, descricao, data)
-    {
-        if (tipo == "saida") _totalDespesas = valor;
-        else throw new ArgumentNullException();
-
-    }
-    protected double TotalDespesas()
-    {
-        
-        return _totalDespesas;
-    }
-
-    public override string ToString()
-    {
-        return $"Valor total das despesas: {_totalDespesas} reais";
-    }
-
-}
+ public class Despesas : FluxoDeCaixa
+   {
+   
+       public Despesas(string tipo, double valor, string descricao, DateTime data) : base(tipo, valor, descricao, data)
+       {
+           if (tipo != "saida") throw new ArgumentNullException();
+       }
+   
+   }
 }
